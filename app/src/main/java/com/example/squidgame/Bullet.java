68ef40d45882d5ec4d531e7aab5388a6c1afd447
Nour94276 <tounsinourhouda@gmail.com>
2021-11-11 15:29:46 +0100

    Add birds & game over conditions

--- app/src/main/java/com/example/squidgame/Bullet.java
+++ app/src/main/java/com/example/squidgame/Bullet.java
@@ -6,16 +6,17 @@ import static com.example.squidgame.GameView.screenRatioY;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Rect;
 
 public class Bullet {
-    int x, y;
+    int x, y, width, height;
     Bitmap bullet;
 
     Bullet(Resources res) {
         bullet = BitmapFactory.decodeResource(res, R.drawable.bullet);
 
-        int width = bullet.getWidth();
-        int height = bullet.getHeight();
+        width = bullet.getWidth();
+        height = bullet.getHeight();
 
         width /= 4;
         height /= 4;
@@ -24,5 +25,9 @@ public class Bullet {
         height *= (int) screenRatioY;
 
         bullet = Bitmap.createScaledBitmap(bullet, width, height, false);
+    }
+
+    Rect getCollisionShape() {
+        return new Rect(x, y, x + width, y + height);
     }
 }
